d61631ca7b5f40e517eb8154df0fce2ea10c1c6a
giuliocollesei <giuliocollesei@gmail.com>
2019-04-23 15:07:12 +0200

    homepage config

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
 
 export const IndexPageTemplate = ({
   image,
@@ -23,7 +23,7 @@ export const IndexPageTemplate = ({
       <h3>Latest stories</h3>
     </section>
   </div>
-);
+)
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -35,23 +35,24 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array
   })
-};
+}
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { frontmatter } = data.markdownRemark
 
-  return (
-    <IndexPageTemplate
-      image={frontmatter.image}
-      title={frontmatter.title}
-      heading={frontmatter.heading}
-      subheading={frontmatter.subheading}
-      mainpitch={frontmatter.mainpitch}
-      description={frontmatter.description}
-      intro={frontmatter.intro}
-    />
-  );
-};
+  return <p>placeholder</p>
+  // return (
+  //   <IndexPageTemplate
+  //     image={frontmatter.image}
+  //     title={frontmatter.title}
+  //     heading={frontmatter.heading}
+  //     subheading={frontmatter.subheading}
+  //     mainpitch={frontmatter.mainpitch}
+  //     description={frontmatter.description}
+  //     intro={frontmatter.intro}
+  //   />
+  // );
+}
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -59,44 +60,16 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object
     })
   })
-};
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
-`;
+`
