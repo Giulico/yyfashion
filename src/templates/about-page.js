41504e4b7611dce638d7b8484d13c5289e269bcc
giuliocollesei <giuliocollesei@gmail.com>
2019-05-03 11:23:48 +0200

    removed Content import form About page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,10 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { graphql } from "gatsby";
-import Content, { HTMLContent } from "../components/Content";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
+  // const PageContent = contentComponent || Content;
 
   return (
     <section className="section section--gradient">
@@ -15,23 +14,23 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
+              {/* <PageContent className="content" content={content} /> */}
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func
-};
+}
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data;
+  const { markdownRemark: post } = data
 
   return (
     <AboutPageTemplate
@@ -39,14 +38,14 @@ const AboutPage = ({ data }) => {
       title={post.frontmatter.title}
       content={post.html}
     />
-  );
-};
+  )
+}
 
 AboutPage.propTypes = {
   data: PropTypes.object.isRequired
-};
+}
 
-export default AboutPage;
+export default AboutPage
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -57,4 +56,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`;
+`
