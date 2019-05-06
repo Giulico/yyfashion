b02e542d0bfeb7f30ef16814b39b1e80c5b9e829
giuliocollesei <giuliocollesei@gmail.com>
2019-05-06 12:52:25 +0200

    updated gatsby-ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,13 +6,6 @@ import './src/styles/app.css'
 // Store
 import store from './src/store'
 
-import Logo from './src/components/Logo'
-// Img
-import LogoImg from './src/assets/images/yy-logo.jpg'
 export const wrapRootElement = ({ element }) => {
-  return (
-    <>
-      <Provider store={store}>{element}</Provider>
-    </>
-  )
+  return <Provider store={store}>{element}</Provider>
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Provider from 'redhooks'
-import { ParallaxProvider } from 'react-scroll-parallax'
+
+import './src/styles/app.css'
 
 // Store
 import store from './src/store'
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <Provider store={store}>
-      <ParallaxProvider>{element}</ParallaxProvider>
-    </Provider>
-  )
+  return <Provider store={store}>{element}</Provider>
 }
