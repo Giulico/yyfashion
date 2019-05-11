899b57d167f40de840c8c6e8d14b3ca857e28e86
giuliocollesei <giuliocollesei@gmail.com>
2019-05-11 09:25:58 +0200

    About page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
 // Components
+import { Grid, GridItem } from '../components/Grid'
 import Container from '../components/Container'
 
 const AboutPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter, html } = data.markdownRemark
   const { title, intro, body } = frontmatter
-  console.log(frontmatter)
 
   return (
     <Container>
-      <h1>{title}</h1>
-      <h2>{intro}</h2>
-      <p>{body}</p>
+      <Grid>
+        <GridItem large>
+          <h1>{title}</h1>
+          <p className="u-text--large">{intro}</p>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </GridItem>
+      </Grid>
     </Container>
   )
 }
@@ -32,7 +36,6 @@ export const aboutPageQuery = graphql`
       frontmatter {
         title
         intro
-        body
       }
     }
   }
