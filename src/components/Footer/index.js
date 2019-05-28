23f5df60ccfb97da480d0c68e733ff9a51b387c8
giuliocollesei <giuliocollesei@gmail.com>
2019-05-28 08:20:17 +0200

    Corretta ragione sociale

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ import style from './Footer.module.css'
 
 const Footer = () => (
   <footer className={style.root}>
-    Yellow & Yellow Fashion - Via Venezia, 17 - 35027 Noventa Padovana (PD) -{' '}
-    P.Iva 02489470282 |&nbsp;<Link to="/cookie">Note legali</Link>&nbsp;|&nbsp;
+    Yellow & Yellow Srl - Via Venezia, 17 - 35027 Noventa Padovana (PD) - P.Iva
+    02489470282 |&nbsp;<Link to="/cookie">Note legali</Link>&nbsp;|&nbsp;
     <Link to="/privacy">Privacy</Link>
   </footer>
 )
